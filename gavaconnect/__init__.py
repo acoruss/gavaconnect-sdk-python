5e0d8671f2fe6f1db75317a6bf97ef2dd225c3ba
Musale Martin <martinmshale@gmail.com>
2025-08-21 15:13:36 +0300

    Add minimal imports

--- gavaconnect/__init__.py
+++ gavaconnect/__init__.py
@@ -1,5 +1,18 @@
 """GavaConnect SDK for Python."""
 
+from ._version import __version__
 from .checkers import KRAPINChecker
+from .config import SDKConfig
+from .errors import APIError, RateLimitError, SDKError, TransportError
 
-__all__ = ["KRAPINChecker"]
+__all__ = [
+    "__version__",
+    "SDKConfig",
+    "SDKError",
+    "APIError",
+    "RateLimitError",
+    "TransportError",
+    "AsyncGavaConnect",
+    "GavaConnect",
+    "KRAPINChecker",
+]
