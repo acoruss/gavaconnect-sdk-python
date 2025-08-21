5186d3413fa5f7608fb5aa469cf46e32feae6c2b
Musale Martin <martinmshale@gmail.com>
2025-08-21 12:14:15 +0300

    Remove unused main tests

--- gavaconnect/__init__.py
+++ gavaconnect/__init__.py
@@ -1,10 +1,4 @@
 """GavaConnect SDK for Python."""
 from .checkers import KRAPINChecker
 
-
-def main() -> None:
-    """Main entry point for the CLI."""
-    print("Hello from gavaconnect-sdk-python!")
-
-
-__all__ = ["main", "KRAPINChecker"]
+__all__ = ["KRAPINChecker"]
